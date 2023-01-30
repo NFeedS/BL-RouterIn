20b5b6fd049d0cb2090bedea5ff6fc8a8371de04
Leonardo Malcotti <leonardo.malcotti@studenti.unitn.it>
2023-01-30 15:29:19 +0100

    spring init

---
+++ src/main/java/com/nfeeds/business/routerin/RouterinApplication.java
@@ -0,0 +1,13 @@
+package com.nfeeds.business.routerin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RouterinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RouterinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nfeeds/business/routerin/RouterinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nfeeds.business.routerin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RouterinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
